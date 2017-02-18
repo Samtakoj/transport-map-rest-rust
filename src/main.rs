6024ee9531d70d574886ebec6f043c5c1e7629ff
Artsiom <artkostm@gmail.com>
2017-02-18 16:28:30 +0300

    added son support

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
 #[macro_use] extern crate nickel;
+#[macro_use] extern crate serde_json;
 
 use nickel::Nickel;
 
+use serde_json::{
+    to_string_pretty,
+};
+
 fn main() {
     let mut server = Nickel::new();
+    let host_port = "127.0.0.1:6767";
+
+    let json_response = json!({
+        "name" : "Transport-map-rest-rust",
+        "version" : "0.0.1",
+        "location" : host_port,
+        "status" : "GREEN"
+    });
 
     server.utilize(router! {
         get "**" => |_req, _res| {
-            "Hello world!"
+            to_string_pretty(&json_response).unwrap()
         }
     });
 
-    server.listen("127.0.0.1:6767");
+    server.listen(host_port);
 }
